760a6dbd588de7edefe77ae92e792d0f8b5c9ae2
Mohamed Irfan <i4irfan777@gmail.com>
2023-11-08 02:25:43 +0530

    infix postfix conversions

---
+++ s3/dsa/infixpostfix.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stack
+{
+  int top;
+  int size;
+  int arr[100];
+} stack;
+
+stack *createStack();
+void push(stack *s, int data);
+void pop(stack *s);
+void infixToPostfix(char *infix, stack *s);
+void postfixToInfix(char *postfix, stack *s);
+
+int main()
+{
+  printf("\nEnter the expression: ");
+  char exp[100];
+  gets(exp);
+  stack *s = createStack();
+  printf("1)infix to postfix\n2)postfix to infix\nChoice: ");
+  int ch;
+  scanf("%d", &ch);
+  if (ch == 1)
+  {
+    infixToPostfix(exp, s);
+  }
+  else if (ch == 2)
+  {
+    postfixToInfix(exp, s);
+  }
+  else
+  {
+    printf("Invalid choice\n");
+  }
+}
+
+void infixToPostfix(char *infix, stack *s)
+{
+  int j = 0;
+  for (j = 0; infix[j] != '\0'; j++)
+    ;
+  infix[j] = ')';
+  for (int i = 0; i <= j; i++)
+  {
+    if (infix[i] >= 'a' && infix[i] <= 'z')
+    {
+      printf("%c", infix[i]);
+    }
+    else if (infix[i] == '(')
+    {
+      push(s, infix[i]);
+    }
+    else if (infix[i] == ')')
+    {
+      while (s->top >= 0 && s->arr[s->top] != '(')
+      {
+        printf("%c", s->arr[s->top]);
+        pop(s);
+      }
+      if (s->top >= 0)
+        pop(s);
+    }
+    else
+    {
+      if (infix[i] == '+' || infix[i] == '-')
+      {
+        while (s->arr[s->top] == '*' || s->arr[s->top] == '/' || s->arr[s->top] == '^')
+        {
+          printf("%c", s->arr[s->top]);
+          pop(s);
+        }
+        push(s, infix[i]);
+      }
+      else if (infix[i] == '*')
+      {
+        while (s->arr[s->top] == '/' || s->arr[s->top] == '^')
+        {
+          printf("%c", s->arr[s->top]);
+          pop(s);
+        }
+        push(s, infix[i]);
+      }
+      else if (infix[i] == '/')
+      {
+        while (s->arr[s->top] == '^')
+        {
+          printf("%c", s->arr[s->top]);
+          pop(s);
+        }
+        push(s, infix[i]);
+      }
+      else if (infix[i] == '^')
+      {
+        push(s, infix[i]);
+      }
+      else
+      {
+        printf("Invalid character\n");
+        return;
+      }
+    }
+  }
+}
+
+void postfixToInfix(char *postfix, stack *s)
+{
+  int j = 0;
+  for (j = 0; postfix[j] != '\0'; j++)
+    ;
+
+  for (int i = 0; i < j; i++)
+  {
+    printf("%c", postfix[i]);
+  }
+  for (int i = 0; i < j; i++)
+  {
+    int c = postfix[i] - '0';
+    if (postfix[i] == ' ')
+      continue;
+    if (c >= 0 && c <= 9)
+    {
+      int val = 0;
+      while (i < j && postfix[i] >= '0' && postfix[i] <= '9')
+      {
+        val = val * 10 + (postfix[i] - '0');
+        i++;
+      }
+      i--;
+      push(s, val);
+    }
+    else
+    {
+      int a = s->arr[s->top];
+      pop(s);
+      int b = s->arr[s->top];
+      pop(s);
+      if (postfix[i] == '+')
+      {
+        push(s, a + b);
+      }
+      else if (postfix[i] == '-')
+      {
+        push(s, a - b);
+      }
+      else if (postfix[i] == '*')
+      {
+        push(s, a * b);
+      }
+      else if (postfix[i] == '/')
+      {
+        push(s, a / b);
+      }
+      else
+      {
+        printf("Invalid character\n");
+        return;
+      }
+    }
+  }
+  printf("\nExpression = %d\n", s->arr[s->top]);
+}
+
+stack *createStack()
+{
+  stack *s = (stack *)malloc(sizeof(stack));
+  s->top = -1;
+  s->size = 100;
+  return s;
+}
+
+void push(stack *s, int data)
+{
+  if (s->top == s->size - 1)
+  {
+    printf("Stack Overflow\n");
+    return;
+  }
+  s->top++;
+  s->arr[s->top] = data;
+}
+
+void pop(stack *s)
+{
+  if (s->top < 0)
+  {
+    printf("Stack underflow\n");
+    return;
+  }
+  s->top--;
+}
