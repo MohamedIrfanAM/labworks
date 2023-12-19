fb59c480d6763b094a70a798480d3de0824481de
Mohamed Irfan <i4irfan777@gmail.com>
2023-12-19 18:12:33 +0530

    Refactor BinaryHeap class and add sorting functionality

--- s3/oops/cycle_6/q4.java
+++ s3/oops/cycle_6/q4.java
@@ -1,93 +1,70 @@
 import java.util.Scanner;
 
-class BinaryHeap{
+class BinaryHeap {
   int size;
   int root;
   int tail;
   int[] arr;
-  BinaryHeap(int size)
-  {
+
+  BinaryHeap(int size) {
     this.size = size;
     this.arr = new int[size];
     this.tail = -1;
     this.root = 0;
   }
-  public void heapify(int loc){
+
+  public void heapify(int loc) {
     int largest = loc;
-    int left = ((loc+1)*2)-1;
-    int right = ((loc+1)*2);
-    if(left <= tail && arr[left] > arr[largest] ){
-      int temp = arr[left];
-      arr[left] = arr[largest];
-      arr[largest] = temp;
+    int left = ((loc + 1) * 2) - 1;
+    int right = ((loc + 1) * 2);
+    if (left <= tail && arr[left] > arr[largest]) {
       largest = left;
     }
-    if(right <= tail && arr[right] > arr[largest] ){
-      int temp = arr[right];
-      arr[right] = arr[largest];
-      arr[largest] = temp;
+    if (right <= tail && arr[right] > arr[largest]) {
       largest = right;
     }
-    if(largest != loc){
+    if (largest != loc) {
+      int temp = arr[loc];
+      arr[loc] = arr[largest];
+      arr[largest] = temp;
       heapify(largest);
     }
   }
-  public void insert(int data){
-    tail++;
-    arr[tail] = data;
-    int parent = tail/2;
-    int child = tail;
-    while(child > 0){
-      if(arr[parent] < arr[child]){
-        int temp = arr[parent];
-        arr[parent] = arr[child];
-        arr[child] =  temp;
-      }
-      else{
-        heapify(child);
-      }
-      child = parent;
-      parent /= 2;
+
+  void sort() {
+    for (int i = (size + 1) / 2; i >= 0; i--) {
+      heapify(i);
+    }
+    for (int i = tail; i >= 0; i--) {
+      int temp = arr[0];
+      arr[0] = arr[i];
+      arr[i] = temp;
+      tail--;
+      heapify(0);
     }
   }
-  public void delete(){
-    if(tail == -1){
-      System.out.println("Heap underflow!!");
-      return;
+
+  void display() {
+    for (int i = 0; i < size; i++) {
+      System.out.print(arr[i] + " ");
     }
-    arr[root] = arr[tail];
-    tail--;
-    heapify(0);
-  }
-  public void display(){
-    if(tail == -1){
-      System.out.println("Heap is empty!!");
-      return;
-    }
-    int[] d = arr;
-    int t = tail;
-    System.out.println("");
-    for(int i = 0; i <= t; i++){
-      System.out.print(this.arr[root] + " ");
-      delete();
-    }
-    System.out.println("");
-    this.arr = d;
+    System.out.print("\n");
   }
 }
 
-class q4{
-  public static void main(String[] args)
-  {
+class q4 {
+  public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter size of the heap: ");
-    int n =  sc.nextInt(); 
+    int n = sc.nextInt();
     BinaryHeap h = new BinaryHeap(n);
-    for(int i = 0; i< n; i++){
-      System.out.print("\nEnter element " + (i+1) + " : ");
+    System.out.print("\nEnter elements of the heap: ");
+    for (int i = 0; i < n; i++) {
       int x = sc.nextInt();
-      h.insert(x);
+      h.arr[++h.tail] = x;
     }
+    h.sort();
     h.display();
+    sc.close();
   }
 }
