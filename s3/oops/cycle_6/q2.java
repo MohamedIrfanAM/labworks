4fbd39dbe72fe69fe12f3c307feb24f46c3d90f5
Mohamed Irfan <i4irfan777@gmail.com>
2023-12-19 17:56:44 +0530

    Refactor q2.java: Improve code formatting and variable naming

--- s3/oops/cycle_6/q2.java
+++ s3/oops/cycle_6/q2.java
@@ -1,33 +1,31 @@
 import java.util.Scanner;
 
-class q2{
-  public static void main(String[] args){
+class q2 {
+  public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter size of the array: ");
     int n = sc.nextInt();
-    int l=-1;
-    int r=n;
+    int l = -1;
+    int r = n;
     int[] a = new int[n];
-    for(int i= 0; i < n;i++){
+    for (int i = 0; i < n; i++) {
       a[i] = sc.nextInt();
     }
     System.out.print("\nEnter data to search: ");
     int data = sc.nextInt();
-    while(r-l> 1){
-      int m = l + (r-l)/2;
-      if(a[m] <= data){
+    while (r - l > 1) {
+      int m = l + (r - l) / 2;
+      if (a[m] <= data) {
         l = m;
-      }
-      else{
+      } else {
         r = m;
       }
     }
-    if(a[l] == data){
+    if (a[l] == data) {
       System.out.println("Data found at - " + l);
-    }
-    else{
+    } else {
       System.out.println("Data not found");
     }
-      
+    sc.close();
   }
 }
