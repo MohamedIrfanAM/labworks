af4ceeb74c55c300fafcce9e888785bd66a73408
Mohamed Irfan <i4irfan777@gmail.com>
2023-12-19 18:12:26 +0530

    Refactor quickSort method in q3.java

--- s3/oops/cycle_6/q3.java
+++ s3/oops/cycle_6/q3.java
@@ -1,28 +1,29 @@
 import java.util.Scanner;
 
-class q3{
+class q3 {
 
-  public static void quickSort(String [] arr,int l,int r,int n){
-    if(l >= r) return;
+  public static void quickSort(String[] arr, int l, int r, int n) {
+    if (l >= r)
+      return;
     int p = l;
     int ll = l;
     int rr = r;
 
     l++;
-    while(l < r){
-      if(arr[l].compareTo(arr[p]) <= 0){
+    while (l < r) {
+      if (arr[l].compareTo(arr[p]) <= 0) {
         l++;
         continue;
       }
-      if(arr[p].compareTo(arr[r]) >= 0){
+      if (arr[p].compareTo(arr[r]) >= 0) {
         String temp = arr[l];
         arr[l] = arr[r];
         arr[r] = temp;
         l++;
       }
       r--;
-    } 
-    if(arr[l].compareTo(arr[p]) > 0){
+    }
+    if (arr[l].compareTo(arr[p]) > 0) {
       l--;
     }
     String temp = arr[l];
@@ -32,32 +33,31 @@ class q3{
     p = l;
     l = x;
 
-    if(p != l){
-      quickSort(arr,ll,p-1,n);
-      quickSort(arr,p+1,rr,n);
-    }
-    else{
-      quickSort(arr,ll+1,rr,n);
+    if (p != l) {
+      quickSort(arr, ll, p - 1, n);
+      quickSort(arr, p + 1, rr, n);
+    } else {
+      quickSort(arr, ll + 1, rr, n);
     }
   }
 
-  public static void main(String[] args)
-  {
+  public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter size of the array: ");
-    int n =  sc.nextInt(); 
+    int n = sc.nextInt();
     String[] arr = new String[n];
     sc.nextLine();
-    for(int i = 0; i < n; i++){
-      System.out.print("\nEnter element " + (i+1) + " : ");
+    for (int i = 0; i < n; i++) {
+      System.out.print("\nEnter element " + (i + 1) + " : ");
       arr[i] = sc.nextLine();
     }
 
-    quickSort(arr , 0 ,n-1,n);
+    quickSort(arr, 0, n - 1, n);
 
     System.out.println("\nSorted array = ");
-    for(int i=0; i< n; i++){
+    for (int i = 0; i < n; i++) {
       System.out.print(arr[i] + " ");
     }
+    sc.close();
   }
 }
